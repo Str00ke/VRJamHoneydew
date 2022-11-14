629cdb06fcb21a7458eb503f54198b87610f56f1
Str0ke_ <charlelie.garrigues@gmail.com>
2022-11-14 11:31:39 +0100

    Start enemies

---
+++ VRJam/Assets/Enemies/Enemy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private EnemyData _data;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
